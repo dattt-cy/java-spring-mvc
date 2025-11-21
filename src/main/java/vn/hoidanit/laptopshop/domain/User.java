91a353a09d270aa9ec2b67b109387bffb40b4ee5
dattt-cy <victomblack1602@gmail.com>
2025-11-21 22:12:06 +0700

    "Them cac entity cho model user, product, order, order detail, role"

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -1,11 +1,18 @@
 package vn.hoidanit.laptopshop.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,8 +23,14 @@ public class User {
     private String address;
     private String phone;
     private String avatar;
+    // User
+    // User many -> to one ->
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
 
-    // roleId
+    @OneToMany(mappedBy = "user")
+    List<Order> orders;
 
     public String getEmail() {
         return email;
@@ -80,7 +93,5 @@ public class User {
         return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName
                 + ", address=" + address + ", phone=" + phone + ", avatar=" + avatar + "]";
     }
-
-    
 
 }
